97d56fa2491a7ef210c24be520547c1f1d89d58c
Admin <josh.jahans3@gmail.com>
2017-10-16 18:01:15 +0100

    Refactor ErrorFactory to be class

--- src/error/factory.js
+++ src/error/factory.js
@@ -1,26 +1,39 @@
 import { invariant, warning } from '../utils/index'
 
-const messageFactory = ({ message } = {}) => {
-  const messageType = typeof message
-
-  if (messageType === 'string') {
-    message = () => message
-  } else if (messageType === 'function') {
-    const returnType = typeof message()
-
-    if (returnType !== 'string') {
-      throw Error(`Implements: errorFactory: message arg must be string or function that returns a string, instead got: ${messageType}`)
-    }
-  } else {
-    throw Error(`Implements: errorFactory: message arg must be string or function that returns a string, instead got: ${messageType}`)
+class ErrorFactory {
+  constructor({ message }) {
+    this.message = message
   }
 
-  return message
+  get message () {
+    return this._message
+  }
+
+  set message (message) {
+    const messageType = typeof message
+
+    if (messageType === 'string') {
+      message = () => message
+    } else if (messageType === 'function') {
+      const returnType = typeof message()
+
+      if (returnType !== 'string') {
+        throw Error(`Implements: errorFactory: message arg must be string or function that returns a string, instead got: ${messageType}`)
+      }
+    } else {
+      throw Error(`Implements: errorFactory: message arg must be string or function that returns a string, instead got: ${messageType}`)
+    }
+
+    this._message = message
+  }
+
+  warn (...args) {
+    warning(false, this.message(...args))
+  }
+
+  throw (...args) {
+    invariant(false, this.message(...args))
+  }
 }
 
-export default ({ message } = {}) => new (function () {
-  this.message = messageFactory({ message })
-  this.warn = (...args) => { warning(false, this.message(...args)) }
-  this.throw = (...args) => { invariant(false, this.message(...args)) }
-  return this
-})()
+export default ({ message } = {}) => new ErrorFactory({ message })
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 export { default } from './implements'
 export { default as Interface } from './Interface'
-export { default as type } from './type'
+// export { default as type } from './type'
 export { IMPLEMENTS_TYPES } from './constants'
+
+import type from './type'
+
+console.log(type('banana'))
