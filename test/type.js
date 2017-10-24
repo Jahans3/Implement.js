06163a9b46cc7b2ce9914082c5e1aca4ae9afa39
Admin <josh.jahans3@gmail.com>
2017-10-24 18:23:07 +0100

    Add tests for typeObject

--- test/type.js
+++ test/type.js
@@ -1,9 +1,10 @@
 import { expect } from 'chai'
-import { isValidType } from '../src/type'
+import { isValidType, typeObject } from '../src/type'
+import { IMPLEMENT_TYPES } from "../src/constants"
 
 describe('type', () => {
   describe('isValidType', () => {
-    it('will return the given type if it is valid', done => {
+    it('should return the given type if it is valid', done => {
       const validType = 'string'
       const checkTypeIsValid = isValidType({ type: validType })
 
@@ -12,7 +13,7 @@ describe('type', () => {
       done()
     })
 
-    it('will return false if an invalid type is given', done => {
+    it('should return false if an invalid type is given', done => {
       const invalidType = 'banana'
       const checkTypeIsValid = isValidType({ type: invalidType })
 
@@ -21,8 +22,25 @@ describe('type', () => {
       done()
     })
   })
+
   describe('typeObject', () => {
-    // ...
+    it('should return an object with a unique key indicating it is a type object used by this library', done => {
+      const convertToTypeObject = { type: 'number' }
+      const convertedTypeObject = typeObject(convertToTypeObject)
+
+      expect(convertedTypeObject[IMPLEMENT_TYPES.TYPE]).to.equal(true)
+
+      done()
+    })
+
+    it('should convert the shape argument into an Internet property', done => {
+      const convertToTypeObject = { type: 'object', shape: { test: true } }
+      const convertedTypeObject = typeObject(convertToTypeObject)
+
+      expect(convertedTypeObject.Interface).to.equal(convertToTypeObject.shape)
+
+      done()
+    })
   })
   describe('type', () => {
     // ...
