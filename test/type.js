dea02c6652d1ecd63e1aca5bd321308777c22ad4
Admin <josh.jahans3@gmail.com>
2017-10-24 18:10:24 +0100

    Add test for isValidType

--- test/type.js
+++ test/type.js
@@ -4,7 +4,12 @@ import { isValidType } from '../src/type'
 describe('type', () => {
   describe('isValidType', () => {
     it('will return the given type if it is valid', done => {
+      const validType = 'string'
+      const checkTypeIsValid = isValidType({ type: validType })
 
+      expect(checkTypeIsValid).to.equal('string')
+
+      done()
     })
   })
   describe('typeObject', () => {
