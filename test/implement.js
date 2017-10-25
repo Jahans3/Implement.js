3d99ef37bd9225346fadfdecaf9dbc25e2c1bea2
Admin <josh.jahans3@gmail.com>
2017-10-25 17:18:36 +0100

    Add imports and formatting

--- test/implement.js
+++ test/implement.js
@@ -1,18 +1,26 @@
 import { expect } from 'chai'
+import type from '../src/type'
+import Interface from '../src/Interface'
+import implement from '../src/implement'
+import { IMPLEMENT_TYPES } from "../src/constants"
 
 describe('implement', () => {
   describe('trimProperty', () => {
     // ...
   })
+
   describe('getType', () => {
     // ...
   })
+
   describe('implementTypedArray', () => {
     // ...
   })
+
   describe('implementType', () => {
     // ...
   })
+
   describe('implement', () => {
     // ...
   })
