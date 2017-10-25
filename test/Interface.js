3687f503407d4dfe3de6700020d2be945f32ff25
Admin <josh.jahans3@gmail.com>
2017-10-25 17:15:44 +0100

    interface tests complete

--- test/Interface.js
+++ test/Interface.js
@@ -1,5 +1,35 @@
 import { expect } from 'chai'
+import Interface from '../src/Interface'
+import type from '../src/type'
+import { IMPLEMENT_TYPES } from "../src/constants"
 
 describe('Interface', () => {
-  // ...
+  it('should throw an \'InvalidInterface\' error if an Interface is given a property that is not a valid type object', done => {
+    const expectedError = 'Invalid object given as Interface property, must be a valid type() object.'
+
+    try {
+      Interface('Test')({
+        name: type('string'),
+        count: 'number'
+      })
+    } catch (err) {
+      expect(err instanceof Error).to.equal(true)
+      expect(err.message).to.equal(expectedError)
+      done()
+    }
+  })
+
+  it('should return a valid Interface object with options, a name, and a flag', done => {
+    const interfaceName = 'Test'
+    const interfaceOptions = { trim: true, strict: true, error: true, warn: false }
+    const MyInterface = Interface(interfaceName)({
+      count: type('number'),
+      message: type('string')
+    }, interfaceOptions)
+
+    expect(MyInterface[IMPLEMENT_TYPES.INTERFACE]).to.equal(true)
+    expect(MyInterface[IMPLEMENT_TYPES.NAME]).to.equal(interfaceName)
+    expect(MyInterface[IMPLEMENT_TYPES.OPTIONS]).to.deep.equal(interfaceOptions)
+    done()
+  })
 })
