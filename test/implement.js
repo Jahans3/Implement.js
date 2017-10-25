b0e96ce3da96b9fc9e88a61f9c0d90c80b56b10a
Admin <josh.jahans3@gmail.com>
2017-10-25 17:23:42 +0100

    First trimProperty test complete

--- test/implement.js
+++ test/implement.js
@@ -1,12 +1,22 @@
 import { expect } from 'chai'
 import type from '../src/type'
 import Interface from '../src/Interface'
-import implement from '../src/implement'
+import implement, { trimProperty } from '../src/implement'
 import { IMPLEMENT_TYPES } from "../src/constants"
 
 describe('implement', () => {
   describe('trimProperty', () => {
-    // ...
+    it('should delete a property from an object', done => {
+      const interfaceName = 'Test'
+      const property = 'testprop'
+      const object = { [property]: true }
+
+      trimProperty({ object, property, interfaceName })
+
+      expect(object[property]).to.equal(undefined)
+      done()
+    })
+    // it('should emit a warning showing which property was being deleted from an Interface', done => {})
   })
 
   describe('getType', () => {
