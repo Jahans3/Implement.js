bb6c2583c5382a9e31e59fdfb12f35479d27b620
Admin <josh.jahans3@gmail.com>
2017-10-26 00:45:55 +0100

    add uuid pattern

--- src/constants.js
+++ src/constants.js
@@ -28,4 +28,4 @@ export const VALID_TYPES_LIST = (() => {
   return validTypesList
 })()
 
-export const MATCH_UUID = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+export const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
